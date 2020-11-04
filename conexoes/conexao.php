a2f304de1643f9660755dfac26ad02ea452d4265
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2020-11-04 01:10:37 -0300

    Correção de parâmetros de acesso e encerramento do banco de dados

--- conexoes/conexao.php
+++ conexoes/conexao.php
@@ -2,7 +2,7 @@
 
     $servidor = 'localhost';
     $usuario  = 'root';
-    $senha    = 'root';
+    $senha    = '';
     $banco    = 'eventos';
 
     $conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
